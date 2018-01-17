b735da51ed6601a738c83ed86d1ab50c84324db4
dsteoh <desiangteoh@gmail.com>
2018-01-17 12:38:44 +0800

    linq and IEnum changes to solution 2

--- Vidly2/Controllers/CustomerController.cs
+++ Vidly2/Controllers/CustomerController.cs
@@ -10,28 +10,37 @@ namespace Vidly2.Controllers
 {
     public class CustomerController : Controller
     {
-        private List<Customer> customers = new List<Customer>
-        {
-            new Customer() {Id = 1, Name = "Alex"},
-            new Customer() {Id = 2, Name = "John"}
-        };
         // GET: Customer
         public ActionResult ListCustomer()
         {
-            var customerList = new CustomerViewModel {Customers = customers};
+            var customerList = GetCustomer();
             return View(customerList);
         }
 
         public ActionResult DetailCustomer(int id)
         {
-            foreach (var cus in customers)
+            //foreach (var cus in customers)
+            //{
+            //    if (cus.Id == id)
+            //    {
+            //        return View(cus);
+            //    }
+            //}
+            //return HttpNotFound();
+            var customer = GetCustomer().SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomer()
+        {
+            return new List<Customer>
             {
-                if (cus.Id == id)
-                {
-                    return View(cus);
-                }
-            }
-            return HttpNotFound();
+                new Customer() {Id = 1, Name = "Alex"},
+                new Customer() {Id = 2, Name = "John"}
+            };
+
         }
     }
 }
